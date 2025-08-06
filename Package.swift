ee61b4d82baa5eaff6b089d6ffd31d0a3476ea4a
Wilson Chiviti <wilsonchiviti@gmail.com>
2025-08-06 10:36:28 +0200

    feat: Package.swift: Update dependencies and platform
    This commit updates dependencies in Package.swift and sets the iOS platform to v13. Also updates deployment target of Example project to iOS 13 and modifies provisioning settings. Temporarily removed some dependencies due to disk space.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,33 @@
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
-	name: "FolioReaderKit",
-	dependencies: [
-		.Package(url: "https://github.com/ZipArchive/ZipArchive.git", majorVersion: 2, minor: 1),
-		.Package(url: "https://github.com/cxa/MenuItemKit.git", majorVersion: 3, minor: 0),
-		.Package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", majorVersion: 0, minor: 6),
-		.Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 2),
-		.Package(url: "https://github.com/ArtSabintsev/FontBlaster.git", majorVersion: 4, minor: 0),
-		.Package(url: "https://github.com/fantim/JSQWebViewController.git", majorVersion: 6, minor: 1),
-		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
-	]
+    name: "FolioReaderKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "FolioReaderKit",
+            targets: ["FolioReaderKit"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.5.0"),
+        .package(url: "https://github.com/tadija/AEXML.git", from: "4.6.0"),
+        .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "5.0.0"),
+        // Temporarily removed heavy dependencies due to disk space constraints
+        // You can add back Realm, MenuItemKit, etc. after freeing up disk space
+    ],
+    targets: [
+        .target(
+            name: "FolioReaderKit",
+            dependencies: [
+                "ZipArchive",
+                "AEXML",
+                "FontBlaster",
+            ],
+            path: "Source"
+        ),
+    ]
 )
