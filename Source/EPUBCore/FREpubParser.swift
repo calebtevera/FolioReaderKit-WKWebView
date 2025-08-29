e580e8e18307c8d9746571a83743e26ecf2aa163
caleb <calebtevera@gmail.com>
2025-08-29 08:28:25 +0200

    refactor: Update quote logo handling and improve optional image safety

--- Source/EPUBCore/FREpubParser.swift
+++ Source/EPUBCore/FREpubParser.swift
@@ -473,7 +473,7 @@ class FREpubParser: NSObject, SSZipArchiveDelegate {
     ///
     /// - Parameter url: File URL
     /// - Throws: Error if not possible
-    fileprivate func addSkipBackupAttributeToItemAtURL(_ url: URL) throws {
+    file private func addSkipBackupAttributeToItemAtURL(_ url: URL) throws {
         assert(FileManager.default.fileExists(atPath: url.path))
 
         var urlToExclude = url
